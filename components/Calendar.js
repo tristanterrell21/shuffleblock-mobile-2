42a8b1a08faf339ea9fd1e05f6c793eb6d80af3e
Tristan Terrell <tristanterrell@Tristans-MacBook-Pro.local>
2022-10-04 20:43:15 -0700

    1.0.5

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, StyleSheet, Image, Dimensions} from 'react-native'
+import ImageZoom from 'react-native-image-pan-zoom';
 
 export default class Calendar extends React.Component {
     render () {
         return (
             <View>
-                <Text style={{color:'#ffffff'}}>Calendar</Text>
+                <View style={styles.imageContainer}>
+                    <ImageZoom 
+                        cropWidth={Dimensions.get('window').width * 0.9}
+                        cropHeight={Dimensions.get('window').width * 1.2}
+                        imageWidth={Dimensions.get('window').width * 0.9}
+                        imageHeight={Dimensions.get('window').width * 1.2}>
+                        <Image source={require('../assets/calendarSTA.png')} style={styles.image}></Image>
+                    </ImageZoom>
+                </View>
             </View>
         )
     }
 }
+
+const windowWidth = Dimensions.get('window').width * 0.9
+const windowHeight = Dimensions.get('window').width * 1.2
+
+const styles = StyleSheet.create ({
+    text: {
+        textAlign: 'center',
+        fontSize:24,
+        color: '#ffffff'
+    },
+    imageContainer: {
+        position: 'relative',
+        alignSelf: 'center',
+        top:'5%',
+        left:0,
+        width:windowWidth,
+        height:500
+    },
+    image: {
+        width:windowWidth,
+        height:windowHeight
+    }
+})
